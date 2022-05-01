1b49ba9325fdabf06f2600849e884cd1897d9492
Allen <almcbroombey@hotmail.com>
2022-05-01 01:23:08 -0400

    created and tested functions for index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require(".utils/generateMardown.js");
+const generateMarkdown = require(".utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -20,14 +20,61 @@ const questions = [
         name:"description",
         message:"Please enter a description of your project.",
     },
-    
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter installation instructions for your project.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information.",
+    },
+    {
+        type: "input",
+        name:"contributionGuidelines",
+        message: "Please enter your contribution guidelines.",
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Please enter test instructions for your project.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose license for your project",
+        license: ["MIT", "ISC", "GPL", "Apache", "BSD"],
+    },
+    {
+        type: "input",
+        name: "Username",
+        message: "Enter your GitHub username",
+    },
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "Enter your email address",
+    },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file from generateMarkdown module
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return; console.log(err);
+        }
+        console.log("Successfully generated Markdown");
+    });
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then(function (response) {
+        console.log(response);
+        writeToFile(`${response.title}.md`, response);
+    });
+}
 
 // Function call to initialize app
 init();
